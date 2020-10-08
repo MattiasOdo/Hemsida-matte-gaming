51977dd322e65d6bcc6f90327b586690ee38eae3
MattiasOdo <62542806+MattiasOdo@users.noreply.github.com>
2020-10-08 08:50:43 +0200

    Race game part 1

---
+++ Racing game/Assets/Sprites/sRoad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class sRoad : MonoBehaviour
+{
+    public float speed = 1;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * speed;
+
+        
+        rend.material.mainTextureOffset = new Vector2(0, offset);
+    }
+}
