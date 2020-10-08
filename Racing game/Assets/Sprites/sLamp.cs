024c932a9d3374d7a966a5814dab67e372b99a93
MattiasOdo <62542806+MattiasOdo@users.noreply.github.com>
2020-10-08 11:09:35 +0200

    race game part 2

---
+++ Racing game/Assets/Sprites/sLamp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sLamp : MonoBehaviour
+{
+    public float speed = 1;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * speed;
+        transform.position += Vector3.down * offset;
+    }
+}
---
+++ Racing game/Assets/Sprites/sRighttoLeft.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class sRighttoLeft : MonoBehaviour
+{
+
+    public float delta = 1.5f;
+    public float speed = 2.0f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = startPos;
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
